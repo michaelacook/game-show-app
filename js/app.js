fef96f943cfd093dcb62131290f4cc5605e6f576
Michael Cook <mcook0775@gmail.com>
2020-04-14 13:12:23 -0400

    Improve keydown listener

--- js/app.js
+++ js/app.js
@@ -13,10 +13,14 @@ const phrases = [
 const game = new Game(phrases);
 const startBtn = document.getElementById('btn__reset');
 
+
+// start the game when start button clicked
 startBtn.addEventListener('click', e => {
     game.startGame();
 });
 
+
+// listen for click event, pass letter button to handleInteraction
 document.getElementById('qwerty').addEventListener('click', e => {
     if (e.target.tagName === 'BUTTON') {
         game.handleInteraction(e.target);
@@ -26,8 +30,14 @@ document.getElementById('qwerty').addEventListener('click', e => {
 
 // listen for keydown event, get the letter button corresponding to the key pressed, pass it to handleInteraction
 document.addEventListener('keydown', e => {
-    const keys = document.querySelectorAll('.key');
-    const letter = Array.from(keys)
-        .find(el => el.textContent === String.fromCharCode(e.keyCode).toLowerCase());
-    game.handleInteraction(letter);
+    const validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+                          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+                          'u', 'v', 'w', 'x', 'y', 'z'];
+    const key = String.fromCharCode(e.keyCode).toLowerCase();
+    if (validLetters.includes(key)) {
+        const keys = document.querySelectorAll('.key');
+        const letter = Array.from(keys)
+            .find(el => el.textContent === key);
+        game.handleInteraction(letter);
+    }
 });
