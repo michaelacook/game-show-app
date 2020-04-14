e21db95e59b4a941409b03e0a20febaf857fb053
Michael Cook <mcook0775@gmail.com>
2020-04-14 02:47:26 -0400

    Remove example file

--- js/Game.js
+++ js/Game.js
@@ -4,87 +4,87 @@
 
  class Game 
  {
-     constructor(phrases)
-     {
-        this.missed = 0;
-        this.phrases = phrases;
-        this.activePhrase = null;
-     }
+    constructor(phrases)
+    {
+       this.missed = 0;
+       this.phrases = phrases;
+       this.activePhrase = null;
+    }
 
 
-     startGame()
-     {
-        const phrase = this.getRandomPhrase();
-        phrase.addPhraseToDisplay();
-        this.activePhrase = phrase;
-        document
-            .getElementById('overlay')
-            .style
-            .display = 'none';
-     }
+    startGame()
+    {
+       const phrase = this.getRandomPhrase();
+       phrase.addPhraseToDisplay();
+       this.activePhrase = phrase;
+       document
+          .getElementById('overlay')
+          .style
+          .display = 'none';
+    }
 
 
-     resetGame()
-     {
-        this.missed = 0;
-        const spaces = document
-            .getElementById('phrase')
-            .querySelector('ul')
-            .innerHTML = "";
-        const keys = document
-            .getElementById('qwerty')
-            .getElementsByTagName('button');
-        for (let i = 0; i < keys.length; i++) {
-            if (keys[i].classList.contains('wrong')) {
-                keys[i].classList.remove('wrong');
-            } else if (keys[i].classList.contains('chosen')) {
-                keys[i].classList.remove('chosen');
-            }
-            keys[i].removeAttribute('disabled');
-        }
-        const lifes = document
-            .getElementById('scoreboard')
-            .querySelector('ol')
-            .children;
-        for (let i = 0; i < lifes.length; i++) {
-            lifes[i]
-                .firstElementChild
-                .setAttribute('src', 'images/liveHeart.png');
-        }
-     }
+    resetGame()
+    {
+       this.missed = 0;
+       const spaces = document
+           .getElementById('phrase')
+           .querySelector('ul')
+           .innerHTML = "";
+       const keys = document
+           .getElementById('qwerty')
+           .getElementsByTagName('button');
+       for (let i = 0; i < keys.length; i++) {
+           if (keys[i].classList.contains('wrong')) {
+               keys[i].classList.remove('wrong');
+           } else if (keys[i].classList.contains('chosen')) {
+               keys[i].classList.remove('chosen');
+           }
+           keys[i].removeAttribute('disabled');
+       }
+       const lifes = document
+           .getElementById('scoreboard')
+           .querySelector('ol')
+           .children;
+       for (let i = 0; i < lifes.length; i++) {
+           lifes[i]
+               .firstElementChild
+               .setAttribute('src', 'images/liveHeart.png');
+       }
+    }
 
 
-     getRandomPhrase()
-     {
-        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
-     }
+    getRandomPhrase()
+    {
+       return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    }
 
 
-     handleInteraction(letter)
-     {
-        letter.setAttribute('disabled', "");
-        if (this.activePhrase.checkLetter(letter.textContent)) {
-            letter.classList.add('chosen');
-            this.activePhrase.showMatchedLetter(letter.textContent);
-        } else {
-            letter.classList.add('wrong');
-            this.removeLife();
-        }
-        this.gameOver();
-     }
+    handleInteraction(letter)
+    {
+       letter.setAttribute('disabled', "");
+       if (this.activePhrase.checkLetter(letter.textContent)) {
+           letter.classList.add('chosen');
+           this.activePhrase.showMatchedLetter(letter.textContent);
+       } else {
+           letter.classList.add('wrong');
+           this.removeLife();
+       }
+       this.gameOver();
+    }
 
 
-     removeLife()
-     {
-        this.missed++;
-        const lifes = document 
-            .getElementById('scoreboard')
-            .querySelector('ol')
-            .children;
-        lifes[this.missed - 1]
-            .firstElementChild
-            .setAttribute('src', 'images/lostHeart.png');
-     }
+    removeLife()
+    {
+       this.missed++;
+       const lifes = document 
+           .getElementById('scoreboard')
+           .querySelector('ol')
+           .children;
+       lifes[this.missed - 1]
+           .firstElementChild
+           .setAttribute('src', 'images/lostHeart.png');
+    }
 
 
     checkForWin()
